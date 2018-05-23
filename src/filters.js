eb91779e6fd98e2455431d09783c9172ed2ac149
Liu Xinran <xinran.liu@shopeemobile.com>
2018-05-23 14:44:40 +0800

    dump, destroy, fix filters

--- src/filters.js
+++ src/filters.js
@@ -1,6 +1,9 @@
 module.exports = {
   capitalize: function (value) {
-      value = value.toString()
-      return value.charAt(0).toUpperCase() + value.slice(1)
+    value = value.toString()
+    return value.charAt(0).toUpperCase() + value.slice(1)
+  },
+  uppercase: function(value) {
+    return value.toUpperCase();
   }
 }
