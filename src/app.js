0efc8f615903943dc22cccb165a6a1e6093be0ea
liuxinran <liuxinran1008@gmail.com>
2018-06-19 21:10:13 +0800

    big refactor.. again
    index.html & src/app.js

--- src/app.js
+++ src/app.js
@@ -1,37 +1,48 @@
 import Seed from './main';
 
 Seed.filter('money', function (value) {
-  return '$' + value.toFixed(2)
+  return value ? '$' + value.toFixed(2) : ''
 })
 
-// define a seed
-var list = [
-  {
-    title: 'make this shit kinda work',
-    done: true
-  },
-  {
-    title: 'make this shit work',
-    done: false
-  },
-  {
-    title: 'more feature!',
-    done: false
-  }
-]
-
-var s = Date.now();
-
-var todos = new Seed('#test', {
-  total     : Math.random() * 100000,
-  'msg.wow' : 'wow',
-  hello     : 'hello',
-  todos     : list,
+Seed.controller('TodoList', {
   changeMessage: function () {
-    this.scope['msg.wow'] = 'hola'
+    this.scope.msg = 'It works!'
   },
   remove: function () {
     this.destroy()
+  }
+})
+
+Seed.controller('Todo', {
+  toggle: function () {
+    this.scope.done = !scope.done
+  }
+})
+
+// define a seed
+
+var s = Date.now();
+
+var app = Seed.bootstrap({
+  el: '#app',
+  data: {
+    msg: 'hello!',
+    total: 9999,
+    error: true,
+    todos: [
+        {
+            title: 'hello!',
+            done: true
+        },
+        {
+            title: 'hello!!',
+            done: false
+        },
+        {
+            title: 'hello!!!',
+            done: false
+        }
+    ]
   }
 })
 
